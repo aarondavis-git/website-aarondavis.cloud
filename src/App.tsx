66b426fd03f0970aa801b66a5174c58e23644d16
Aaron-Elihu <65421861+Aaron-Elihu@users.noreply.github.com>
2025-06-09 15:17:31 +0200

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      <nav className="navbar">
+        <h1 className="logo"></h1>
+        <ul className="nav-links">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Projects</a></li>
+          <li><a href="#">Contact</a></li>
+          </ul>
+      </nav>
+      <main className="main-content">
+        <h2>Welcome to My Website</h2>
+        <p>I'm Aaron Davis, a developer and designer building things for the web</p>
+        </main>    
+    </div>
+  );
+
+}
+
+export default App
